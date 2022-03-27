95fcbdd14648a6660a674468e114d440c9946f8d
Marin Marinov <Marin.Marinov38@myhunter.cuny.edu>
2022-03-26 23:17:34 -0400

    more mock and cleanup

--- test/controllers/admin.mock.ts
+++
@@ -1,6 +0,0 @@
-export const ADMIN_MOCK = {
-  firstName: "bobby",
-  lastName: "flay",
-  password: "pass123",
-  email: "bob@gmail.com"
-};
--- test/controllers/adminController.test.ts
+++ test/controllers/adminController.test.ts
@@ -2,29 +2,29 @@ import { Application } from "express";
 import request from "supertest";
 import bootstrap from "../../src/config/bootstrap";
 import { CREATED } from "../../src/config/keys.error";
-import * as db from "./../db";
-import { ADMIN_MOCK } from "./admin.mock";
+import { clearDB, closeDB, connectTestDB } from "./../db";
+import { ADMIN_MOCK, REGISTER_CONTROLLER_SUCCESS } from "./adminController.mock";
 
 describe("Testing Admin Controller", () => {
   const app: Application = bootstrap();
 
   beforeAll(async () => {
-    await db.connectTestDB();
+    await connectTestDB();
   });
 
   afterEach(async () => {
-    await db.clearDB();
+    await clearDB();
   });
 
   afterAll(async () => {
-    await db.closeDB();
+    await closeDB();
   });
 
   test("should register successfully", async () => {
     const admin = ADMIN_MOCK as any;
     admin.confirmPassword = ADMIN_MOCK.password;
     const res = await request(app).post("/register").send(ADMIN_MOCK);
-    expect(res.body).toStrictEqual({ created: true });
+    expect(res.body).toStrictEqual(REGISTER_CONTROLLER_SUCCESS);
     expect(res.status).toBe(CREATED);
   });
 });
