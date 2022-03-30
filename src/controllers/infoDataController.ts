98414608c38300490dc4bbd1a250548205f50e22
Marin Marinov <marin.marinov@macaulay.cuny.edu>
2022-03-29 23:39:50 -0400

    more get routing and remove logs

--- src/controllers/infoDataController.ts
+++ src/controllers/infoDataController.ts
@@ -1,4 +1,13 @@
 import { NextFunction, Request, Response } from "express";
-export function info_data_get(req: Request, res: Response, next: NextFunction) {
-  return res.render("infoData");
+
+export function info_data_create_get(req: Request, res: Response, next: NextFunction) {
+  return res.render("infoData/infoDataCREATE");
+}
+
+export function info_data_edit_get(req: Request, res: Response, next: NextFunction) {
+  return res.render("infoData/infoDataEDIT");
+}
+
+export function info_data_view_get(req: Request, res: Response, next: NextFunction) {
+  return res.render("infoData/infoDataVIEW");
 }
--- src/routes/infoDataRoutes.ts
+++ src/routes/infoDataRoutes.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { infoDataController } from "../controllers";
 const router: Router = Router();
 
-router.get("/infoData", infoDataController.info_data_get);
+router.get("/auth/forms/create", infoDataController.info_data_create_get);
+router.get("/auth/forms/edit", infoDataController.info_data_edit_get);
+router.get("/auth/forms/view", infoDataController.info_data_view_get);
 
 export default router;
